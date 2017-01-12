579a2af3b9c9d8de1f935df4e242f06b0a26af36
jtcurr <johntcurrie@gmail.com>
2017-01-11 20:00:02 -0800

    going to the talk

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,7 +1,9 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
-
+var htmlfetcher = require('../workers/htmlfetcher');
+var handler = require('../web/request-handler');
+var http = require('http');
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
@@ -38,7 +40,7 @@ exports.readListOfUrls = function(webSiteName) {
     arrayUrls[0] = arrayUrls[0].slice(1, ( arrayUrls[0].length ));
 
     arrayUrls[arrayUrls.length - 1] = arrayUrls[arrayUrls.length - 1].slice(0, ( arrayUrls[arrayUrls.length - 1].length - 1 ));
-
+    console.log(arrayUrls);
     exports.isUrlInList(webSiteName, arrayUrls);
 
   });
@@ -46,21 +48,46 @@ exports.readListOfUrls = function(webSiteName) {
 
 exports.isUrlInList = function(webSiteName, listOfUrls) {
   if ( listOfUrls.indexOf(webSiteName) === -1 ) {
-    // call addUrlToList
-    exports.addUrlToList(webSiteName, listOfUrls);
+    exports.downloadUrls(webSiteName);
+    // if it's not there
+      //call downloadUrls
+        //send back loading.html
   } else {
     // call isUrlArchived
     exports.isUrlArchived(webSiteName);
   }
 };
 
-exports.addUrlToList = function(webSiteName, listOfUrls) {
-  // listOfUrls.push(webSiteName);
-  
+exports.addUrlToList = function(webSiteName) {
+  fs.appendFile(__dirname + '/../archives/sites.txt', '\n' + webSiteName, (err) => {
+    if (err) {
+      console.log('ERROR');
+    }
+  });
+
 };
 
 exports.isUrlArchived = function(webSiteName) {
+  //search sites and return html of webSiteName
 };
 
 exports.downloadUrls = function(webSiteName) {
+  htmlfetcher(webSiteName);
+
+
+
+  fs.readFile(__dirname + '/../web/public/loading.html', 'utf-8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    console.log('hello arglebargle');
+    http.createServer( (request, response) => {
+      response.writeHeader(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      response.end();
+    }).listen(8080);
+  });
+  // calls htmlfetcher
+    //calls addUrlToList
+
 };
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,39 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var http = require('http');
+var until = require('util');
+module.exports = function (webSiteName) {
+  // go to webSiteName
+    //retrieve the HTML
+    //add a new file in sites (with webSiteName as name)
+      //make a new file (webSiteName.html) inside webSiteName folder
+  var options = {
+    host: webSiteName,
+    port: 80,
+    path: '/'
+  };
+  var content = '';
+  var req = http.get(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      content += chunk;
+    });
+
+    res.on('end', function () {
+      until.log('hello');
+      // until.log(content);
+    });
+  });
+  req.end();
+
+
+
+
+
+
+  // var htmlFetched = $('html').load(webSiteName);
+  // console.log(htmlFetched);
+
+
+
+};
